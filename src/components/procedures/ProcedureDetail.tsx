d735139fda6a561b34d86f4c200f868c9155515d
Julio Herrera <julio@redciudadana.org.gt>
2025-08-25 23:45:12 -0600

    Fix Scale icon import in ProcedureDetail

--- src/components/procedures/ProcedureDetail.tsx
+++ src/components/procedures/ProcedureDetail.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowLeft, Clock, Building2, User, CheckCircle, AlertCircle, FileText, Users, ChevronRight, MapPin, Phone, Globe, Mail, Lightbulb, MessageSquare, ThumbsUp, Star, Send, List, PlayCircle, ClipboardList, ExternalLink, Info, Lightbulb as LightbulbIcon, MessageCircleIcon, Home } from 'lucide-react';
+import { ArrowLeft, Building2, Clock, User, Users, FileText, CheckCircle, AlertCircle, Star, MessageCircle, ThumbsUp, Calendar, ExternalLink, Phone, Mail, MapPin, Globe, Download, Scale } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Procedure } from '../../types';
 import { useComments } from '../../hooks/useComments';
