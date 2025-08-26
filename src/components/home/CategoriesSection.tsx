ba846df2026b16960b3f5c0e2d5bffca6dc96953
Julio Herrera <julio@redciudadana.org.gt>
2025-08-25 23:34:07 -0600

    Updated CategoriesSection.tsx

--- src/components/home/CategoriesSection.tsx
+++ src/components/home/CategoriesSection.tsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { 
-  ArrowRight,
-  User,
-  Building2,
-  GraduationCap,
-  Heart,
-  Scale,
-  MapPin,
-  FileText,
-  Certificate,
-  Phone,
-  Building,
-  FileCheck,
-  BookOpen,
-  Award,
-  FileHeart,
-  Shield,
-  Gavel,
-  Home,
-  Receipt,
-  Search
-} from 'lucide-react';
+import { ArrowRight, User, Building2, GraduationCap, Heart, Scale, MapPin, FileText, AlignCenterVertical as Certificate, Phone, Building, FileCheck, BookOpen, Award, FileHeart, Shield, Gavel, Home, Receipt, Search } from 'lucide-react';
 import { categories } from '../../data/procedures';
 
 // Create icon map for dynamic access
